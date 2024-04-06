12eba2195e1aba4e29a430ab720a1b3a81979bf5
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-04-06 19:38:39 +0000

    format

--- dice.rb
+++ dice.rb
@@ -17,19 +17,7 @@ BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 #homepage catch-all
 get("/") do
   #"Hello World"
-
-"
-<h1>Dice Roll</h1>
-<h2>Available options for random dice rolls</h2>
-
-<ul>
-  <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-  <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-  <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-  <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-</ul>
-"
-
+  erb(:elephant)
 end
 
 #identify outputs for specific paths beyond homepage
@@ -48,10 +36,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 #GET /dice/2/10 (simulate two 10-sided dice)
